622b0fcf341bbb01432735ad83223a852f677528
reubenadams <reubenjadams@gmail.com>
2025-01-19 21:51:28 +0000

    Modify low_rank_impact.py to also use CIFAR100, and track margin loss for different margin values.

--- low_rank_impact.py
+++ low_rank_impact.py
@@ -1,43 +1,54 @@
 import os
 from copy import deepcopy
-from itertools import product
 
 import torch
 import wandb
 
 from models import LowRankMLP
-from config import low_rank_config
-from data.MNIST.load_data import train_loader, test_loader, get_B
+from config import low_rank_CIFAR100_config as model_config
+from load_data import get_dataloaders, get_B
 
 
-# max_fro_norm = get_B(train_loader)
-max_fro_norm = torch.tensor(28.)  # Reinstate previous line
+train_loader, test_loader = get_dataloaders(model_config.dataset, model_config.batch_size, shrink_test_dataset=True)
+
+max_fro_norm = get_B(train_loader)
+# max_fro_norm = torch.tensor(28.)  # Reinstate previous line
 print(f"Max Frobenius norm of training data: {max_fro_norm.item()}")
 
-wandb.init(project="Low rank impact", name=f"Model dims: {low_rank_config.model_dims}")
+wandb.init(project="Margin loss against margin, CIFAR", name=f"Model dims: {model_config.model_dims}")
 
-model = LowRankMLP(low_rank_config.model_dims, low_rank_config.model_act)
+model = LowRankMLP(model_config.model_dims, model_config.model_act)
 
 
 try:
-    model.load(low_rank_config.model_path)
-    print(f"File {low_rank_config.model_path} found. Loading model...")
+    model.load(model_config.model_path)
+    print(f"File {model_config.model_path} found. Loading model...")
 except FileNotFoundError:
-    print(f"File {low_rank_config.model_path} not found. Training model...")
+    print(f"File {model_config.model_path} not found. Training model...")
     base_train_loss_fn = torch.nn.CrossEntropyLoss(reduction='mean')
     base_test_loss_fn = torch.nn.CrossEntropyLoss(reduction='sum')
-    base_optimizer = torch.optim.Adam(model.parameters(), lr=low_rank_config.learning_rate)
+    base_optimizer = torch.optim.Adam(model.parameters(), lr=model_config.learning_rate)
     model.train(
         train_loss_fn=base_train_loss_fn,
         test_loss_fn=base_test_loss_fn,
         optimizer=base_optimizer,
         train_loader=train_loader,
         test_loader=test_loader,
-        num_epochs=low_rank_config.train_epochs,
+        num_epochs=model_config.train_epochs,
         log_name="base_train_loss",
         get_accuracy=True
         )
-    model.save(low_rank_config.model_path)
+    model.save(model_config.model_path)
+
+
+# Log margin loss of full rank model:
+for margin in torch.linspace(0, 25, 251):
+    margin_loss_logits = model.margin_loss(test_loader, margin, take_softmax=False)
+    wandb.log({"Margin": margin, "Margin loss logits": margin_loss_logits})
+    if margin <= 1:
+        margin_loss_probs = model.margin_loss(test_loader, margin, take_softmax=True)
+        wandb.log({"Margin": margin, "Margin loss probs": margin_loss_probs})
+assert False
 
 
 for rank_comb in model.rank_combs:
@@ -50,5 +61,5 @@ for rank_comb in model.rank_combs:
         min_Ss = model.min_Ss[rank_comb]
         max_Ss = model.max_Ss[rank_comb]
         model.set_to_ranks(rank_comb)
-        margin_loss = model.margin_loss(test_loader, margin=torch.sqrt(torch.tensor(2.)) * eps)
+        margin_loss = model.margin_loss(test_loader, margin=torch.sqrt(torch.tensor(2.)) * eps, take_softmax=True)
         wandb.log({"ranks": rank_comb, "num_params": num_params, "min_UVs": min_UVs, "max_UVs": max_UVs, "min_Ss": min_Ss, "max_Ss": max_Ss, "eps": eps, "margin_loss": margin_loss})
